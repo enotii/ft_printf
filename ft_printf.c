21d4de31009b3305a56039fb434be0fac9f3fa83
Alexandr <fordiablo93@mail.ru>
2019-03-30 20:57:47 +0300

    add flag and width parse

--- ft_printf.c
+++ ft_printf.c
@@ -6,63 +6,119 @@
 /*   By: mbeahan <mbeahan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/29 18:52:56 by mbeahan           #+#    #+#             */
-/*   Updated: 2019/03/29 21:56:41 by mbeahan          ###   ########.fr       */
+/*   Updated: 2019/03/30 20:56:35 by mbeahan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdio.h>
 
-// int     ft_printf_test(const char *format, ...)
-// {
-//     va_list ap;
-//     va_start(ap, format);
+void zeroing_args(t_printf **list)
+{
+    (*list)->minus = 0;
+    (*list)->plus = 0;
+    (*list)->space = 0;
+    (*list)->bar = 0;
+    (*list)->zero = 0;
+    (*list)->width = 0;
+    (*list)->precision = 0;
+    (*list)->size = 0;
+    (*list)->type = 0;
+}
 
-//     if(!strcmp(format, "%d"))
-//     {
-//         int x = va_arg (ap, int);
-// 	printf ("You passed decimal object with value %d\n", x);
-//     }
-//     if(!strcmp(format, "%s"))
-//     {
-//          char *s = va_arg(ap, char *);
-//     printf ("You passed decimal object with value %s\n", s);
-//     }
-//     va_end (ap);
-//     return(0);
+void     parse_flags(char *string, t_printf *list)
+{
+    int i;
+    int check;
 
-int     ft_parse(char *string)
+    check = 0;
+    i = 0;
+    while (string[i] && check < 2)
+    {
+        if (string[i] == '%' && string[i + 1] != '%')
+        {
+            i++;
+            check++;
+        }
+        if (check > 0)
+        {
+            if (string[i] == '+')
+                list->plus = '+';
+            if (string[i] == '-')
+                list->minus = '-';
+            if (string[i] == '0')
+                list->zero = '0';
+            if (string[i] == ' ')
+                list->space = ' ';
+            if (string[i] == '#')
+                list->bar = '#';
+            if (string[i] != '+' && string[i] != '-' && string[i] != '0' && string[i] != ' ' && string[i] != '#')
+                break ;
+        }
+        i++;
+    }
+}
+
+int    parse_width(char *string, t_printf *list)
+{
+    int     i;
+    int     len;
+    char    *width;
+    int     check;
+    int     tmp;
+
+    i = 0;
+    check = 0;
+    len = 0;
+    tmp = 0;
+    while (string[i] && check < 2)
+    {
+        if (string[i] == '%' && string[i + 1] != '%')
+        {
+            i++;
+            check++;
+        }
+        if (string[i] == '+' || string[i] == '-' || string[i] == '0' || string[i] == ' ' || string[i] == '#')
+            i++;
+        if (string[i] > 47 && string[i] < 58)
+        {
+            tmp = i;
+            while(string[i] > 47 && string[i] < 58)
+            {
+                 i++;
+                len++;
+            }
+             break ;
+        }
+        if ((string[i] != '+' && string[i] != '-' && string[i] != '0' && string[i] != ' ' && string[i] != '#') && (!(string[i] > 47 && string[i] < 58)))
+             break ;
+    }
+    width = (char *)malloc(sizeof(char) * len);
+    i = 0;
+    while (i < len)
+    {
+        width[i] = string[tmp];
+        i++;
+        tmp++;
+    }
+    return (ft_atoi(width));
+}
+
+void    ft_parse(char *string)
 {
     int i;
     t_printf *sooqa;
 
     i = 0;
-    while(string[i]!= '%')
+    sooqa = (t_printf *)malloc(sizeof(t_printf));
+    zeroing_args(&sooqa);
+    while(string[i] != '%')
     {
-        ft_puchar(string[i]);
+        ft_putchar(string[i]);
         i++;
     }
-    while(string[i])
-    {
-        if (string[i] == "%")
-        {
-            i++;
-            if (string[i] == '+')
-                sooqa->plus = '+';
-            if (string[i] == '-')
-                sooqa->minus = '-';
-            if (string[i] == '0')
-                sooqa->zero = '0';
-            if (string[i] == ' ')
-                sooqa->space = ' ';
-            if (string[i] == '#')
-                sooqa->oktotorp = '#';
-        }
-        i++;
-    }
-
-
-
+    parse_flags(string, sooqa);
+    sooqa->width = parse_width(string, sooqa);
 }
 int     ft_printf(const char *format, ...)
 {
@@ -70,10 +126,12 @@ int     ft_printf(const char *format, ...)
     i = 0;
     va_list ap;
     va_start(ap,format);
+    ft_parse((char *)format);
     return(0);
 }
 int main()
 {
-    printf("%+-----+d", 123);
+    ft_printf("%+-------+++", 123);
+    //printf("%+-----+d", 123);
     return(0);
 } 
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: mbeahan <mbeahan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/29 18:53:51 by mbeahan           #+#    #+#             */
-/*   Updated: 2019/03/29 21:50:02 by mbeahan          ###   ########.fr       */
+/*   Updated: 2019/03/30 20:48:14 by mbeahan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,23 @@
 #include "libft/libft.h"
 #include <stdarg.h>
 
-typedef struct s_printf
+typedef struct  s_printf
 {
     char    minus;
     char    plus;
     char    space;
-    char    oktotorp;
+    char    bar;
     char    zero;
     int     width;
     int     precision;
     char    size;
     char    type;
-}              t_printf;
+}               t_printf;
+
+void    zeroing_args(t_printf **list);
+void    parse_flags(char *string, t_printf *list);
+void    ft_parse(char *string);
+int     ft_printf(const char *format, ...);
+int     parse_width(char *string, t_printf *list);
+
 #endif
